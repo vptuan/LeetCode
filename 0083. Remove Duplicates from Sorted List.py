494b172b2b2c62cb4f8ec5c310b6e3ec58c6d908
Tuan Vuong <vptuan@users.noreply.github.com>
2026-08-30 17:03:44 +0100

    Implement function to remove duplicates from sorted list

---
+++ 0083. Remove Duplicates from Sorted List.py
@@ -0,0 +1,19 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def deleteDuplicates(self, head):
+        """
+        :type head: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+        curr = head
+        if (curr):
+            while (curr.next):
+                if (curr.val == curr.next.val):
+                    curr.next = curr.next.next
+                else:
+                    curr = curr.next
+        return head
